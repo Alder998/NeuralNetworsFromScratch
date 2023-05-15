b9733c4e9e5961f1c76fd986b5aa0d847d55aaa4
Alder998 <davide.ontano@yahoo.com>
2023-05-15 22:23:00 +0200

    Complete Passage from Notebook

--- NNLibrary.py
+++ NNLibrary.py
@@ -70,3 +70,187 @@ class NeuralNetwork:
         # res[3] = intercetta della rete (un solo valore)
 
         return pd.concat([betaRandom, interceptRandom, wRandom, functionIntRandom], axis=0)
+
+
+    def getPredictions(self, parameterVector, data, dependent, numberOfLayers, numberOfNodes):
+
+        import pandas as pd
+        import numpy as np
+
+        b = parameterVector
+
+        wRandom = np.array(b['value'][b['parameter'].str.contains('W')])
+        interceptRandom = np.array(b['value'][b['parameter'].str.contains('w0')])
+        betaRandom = np.array(b['value'][(b['parameter'].str.contains('B')) & (~b['parameter'].str.contains('B0'))])
+        functionIntRandom = np.array(b['value'][b['parameter'].str.contains('B0')])
+
+        # Costruiamo ogni singolo Nodo usando i dati random e una funzione non lineare reLu
+
+        functionListNode = list()
+        nodeO = 0
+        while nodeO < numberOfNodes:
+            regrWeights = (wRandom[nodeO:nodeO + len(data.columns) - 1])
+            singleFunction = (interceptRandom[nodeO] + (regrWeights * data.loc[:, data.columns != dependent]))
+            reLuFunction = pd.DataFrame(np.where(singleFunction > 0, singleFunction, 0)).set_axis(
+                data.loc[:, data.columns != dependent].columns, axis=1)
+
+            functionListNode.append(reLuFunction)
+
+            nodeO += 1
+
+        # Abbiamo costruito ogni singolo nodo, ora dobbiamo inserirlo nell'equazione generale
+
+        predList = list()
+        for SingleNodeValues in range(len(functionListNode)):
+            predList.append(betaRandom[SingleNodeValues] * functionListNode[SingleNodeValues])
+
+        equationResult = list()
+        for predictorEquationsResults in predList:
+            equationResult.append((functionIntRandom[0] + predictorEquationsResults).transpose().sum().transpose())
+
+        equationResult = pd.concat([series for series in equationResult], axis=1)
+        equationResult = equationResult.transpose().sum().transpose()
+
+        return equationResult
+
+
+    def computeGradient(self, variable, data, numberOfLayers, numberOfNodes):
+
+        import pandas as pd
+        import numpy as np
+
+        b = self.structure()
+
+        if variable == 'B0':
+            return np.full(500000, 1).mean()
+
+        if (variable[0] == 'B') & (variable != 'B0'):
+            wRandom = np.array(
+                b['value'][(b['parameter'].str.contains('W')) & (b['parameter'].str.contains(variable[1]))])
+            wRandom = wRandom[0:len(data.loc[:, data.columns != 'Pred'].columns)]
+
+            interceptRandom = np.array(b['value'][(b['parameter'].str.contains('w0')) &
+                                                  (b['parameter'].str.contains(variable[1]))])
+
+            betaRandom = np.array(b['value'][(b['parameter'].str.contains('B')) & (~b['parameter'].str.contains('B0'))])
+            functionIntRandom = np.array(b['value'][b['parameter'].str.contains('B0')])
+
+            # Costruiamo ogni singolo Nodo usando i dati random e una funzione non lineare reLu
+
+            singleFunction = (interceptRandom[0] + (wRandom * data.loc[:, data.columns != 'Pred']))
+            reLuFunction = pd.DataFrame(np.where(singleFunction > 0, singleFunction, 0)).set_axis(
+                data.loc[:, data.columns != 'Pred'].columns, axis=1)
+
+            return (np.array(reLuFunction)).flatten().mean()
+
+        if (variable[0] == 'w'):
+            wRandom = np.array(
+                b['value'][(b['parameter'].str.contains('W')) & (b['parameter'].str.contains(variable[2]))])
+            wRandom = wRandom[0:len(data.loc[:, data.columns != 'Pred'].columns)]
+
+            interceptRandom = np.array(b['value'][(b['parameter'].str.contains('w0')) &
+                                                  (b['parameter'].str.contains(variable[2]))])
+
+            betaRandom = np.array(b['value'][(b['parameter'].str.contains('B')) & (~b['parameter'].str.contains('B0'))])
+            functionIntRandom = np.array(b['value'][b['parameter'].str.contains('B0')])
+
+            singleFunction = (interceptRandom[0] + (wRandom * data.loc[:, data.columns != 'Pred']))
+            reLuFunction = pd.DataFrame(np.where(singleFunction > 0, 1, 0)).set_axis(
+                data.loc[:, data.columns != 'Pred'].columns, axis=1)
+
+            return (np.array(reLuFunction)).flatten().mean()
+
+        if (variable[0] == 'W'):
+            wRandom = np.array(
+                b['value'][(b['parameter'].str.contains('W')) & (b['parameter'].str.contains(variable[1]))])
+            wRandom = wRandom[0:len(data.loc[:, data.columns != 'Pred'].columns)]
+
+            interceptRandom = np.array(b['value'][(b['parameter'].str.contains('w0')) &
+                                                  (b['parameter'].str.contains(variable[1]))])
+
+            betaRandom = np.array(b['value'][(b['parameter'].str.contains('B')) & (~b['parameter'].str.contains('B0'))])
+            functionIntRandom = np.array(b['value'][b['parameter'].str.contains('B0')])
+
+            singleFunction = (interceptRandom[0] + (wRandom * data.loc[:, data.columns != 'Pred']))
+            toReturn = data.loc[:, data.columns != 'Pred']
+            reLuFunction = pd.DataFrame(np.where(singleFunction > 0, toReturn, 0)).set_axis(
+                data.loc[:, data.columns != 'Pred'].columns, axis=1)
+
+            return (np.array(reLuFunction)).flatten().mean()
+
+    def fit(self, data, dependent, leaningRate, analytics=False):
+
+        import pandas as pd
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import random
+
+        layers = self.numberOfLayers
+        nodes = self.numberOfNodes
+        inputL = self.inputLayer
+
+        leaningRate = leaningRate
+
+        pV = self.structure().reset_index()
+        del [pV['index']]
+
+        trainingEpochs = 1
+        max_iter = 20000
+
+        minimizationPath = list()
+        weights = pV
+        MSEBefore = ((self.getPredictions(pV, data, dependent, layers, nodes) - data[dependent]) ** 2).mean()
+        MSE = 0
+        while (trainingEpochs < max_iter) & (MSEBefore > MSE):
+
+            MSEBefore = (
+                    (self.getPredictions(weights, data, dependent, layers, nodes) - data[dependent]) ** 2).mean()
+
+            gradient = list()
+            for param in range(len(weights['parameter'])):
+                gr = self.computeGradient(weights['parameter'][param], data, layers, nodes)
+                gradient.append(gr)
+                moveGr = -(gr * leaningRate)
+                w_old = weights['value']
+                w_new = pd.concat([weights['parameter'], w_old + moveGr], axis=1).set_axis(['parameter', 'value'],
+                                                                                           axis=1)
+
+            MSE = ((self.getPredictions(w_new, data, dependent, layers, nodes) - data[dependent]) ** 2).mean()
+
+            # print(gradient)
+
+            minimizationPath.append(MSE)
+
+            weights = w_new
+
+            leaningRate = leaningRate * 0.99
+
+            print('Training Epochs:', trainingEpochs)
+            print('Learning Rate', leaningRate)
+            print('\n')
+            print('MSE:', MSE)
+            print('MSE before:', MSEBefore)
+
+            trainingEpochs += 1
+
+        predictionF = pd.concat([self.getPredictions(weights, data, 'Pred', layers, nodes),
+                                 self.getPredictions(pV, data, 'Pred', layers, nodes), data['Pred']],
+                                axis=1).set_axis(['final Weights Prediction', 'Starting Weights', 'True Value'], axis=1)
+        # print(predictionF)
+
+        if analytics == True:
+            minimizationPath = pd.Series(minimizationPath)
+            plt.figure(figsize=(15, 5))
+            plt.scatter(x=minimizationPath.index, y=minimizationPath)
+            plt.title('Minimization Path (no Constraints)')
+            plt.axhline(minimizationPath.min(), color='black', linestyle='dashed')
+
+            plt.figure(figsize=(15, 5))
+            plt.plot((predictionF['final Weights Prediction'] - predictionF['True Value']).cumsum(), color='blue')
+            plt.plot((predictionF['Starting Weights'] - predictionF['True Value']).cumsum(), color='red')
+            plt.title('Trend')
+            plt.axhline(minimizationPath.min(), color='black', linestyle='dashed')
+
+            plt.show()
+
+        return weights
--- Test.py
+++ Test.py
@@ -4,9 +4,28 @@ import numpy as np
 import random
 import NNLibrary
 
+X1, X2, X3, X4, X5, XR = list(), list(), list(), list(), list(), list()
+columns = ['X1', 'X2', 'X3', 'X4', 'X5', 'Pred']
+cR = ['X1', 'X2', 'X3', 'X4', 'X5']
+
+lines = 50000
+for line in range(lines):
+    X1.append(random.random())
+    X2.append(random.random() * random.choice([-2, 2]))
+    X3.append(random.random() * random.choice([-0.5, 0.5]))
+    X4.append(random.random() * random.choice([-3, 3]))
+    X5.append(random.random() * random.choice([-23, 23]))
+    XR.append(random.random() * random.choice([-10, 10]))
+
+simulatedData = pd.concat([pd.Series(X1), pd.Series(X2), pd.Series(X3), pd.Series(X4),
+                           pd.Series(X5), np.abs(pd.Series(XR))], axis=1).set_axis(columns,
+                                                                                   axis=1)
+
 # get Structure
 
-NNt = NNLibrary.NeuralNetwork(5, 1, 5).structure()
+NNt = NNLibrary.NeuralNetwork(6, 1, 9)
+NNFit = NNt.fit(simulatedData, 'Pred', 0.005)
+
 
 
 
