a9c2a08fb2550a520720d12705cb37a265a5d0a6
Alder998 <davide.ontano@yahoo.com>
2023-07-04 23:07:58 +0200

    distribution Approximator (attempt)

--- NeuralNetworks/FunctionApproximation/distributionApproximator.py
+++ NeuralNetworks/FunctionApproximation/distributionApproximator.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+from statsmodels.distributions.empirical_distribution import ECDF
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -27,16 +28,16 @@ SData = SData.set_axis(['ret'], axis = 1)
 
 # Creiamo la nostra mixture con parametri simulati
 
-SData = pd.concat([SData['ret'].cumsum(), pd.DataFrame(np.full(len(SData['ret']),
+SData = pd.concat([SData['ret'], pd.DataFrame(np.full(len(SData['ret']),
             np.random.uniform(size = len(SData['ret'])))).set_axis(['random'], axis = 1) ], axis = 1)
-SData['random'] = SData['random'].cumsum()
+SData['random'] = SData['random']
 
 # Creiamo la rete
 
-structure = [10]
+structure = [100]
 
 NNt = NeuralNetworks.Regression(2, structure)
-NNFit = NNt.fit(SData, 'ret', 10e-3, decreasingRate = 0.99)
+NNFit = NNt.fit(SData, 'ret', 0.008, decreasingRate = 0.99)
 
 prediction = NNt.getPredictions2(NNFit, SData, 'ret')
 #print(prediction)
@@ -47,6 +48,31 @@ plt.figure(figsize=(12, 4))
 plt.plot(SData['ret'], color = 'blue')
 plt.plot(prediction, color = 'red')
 plt.show()
+
+# Creazione della Empirical Cumulative Distribution
+
+ret = SData['ret']
+e_cum = ECDF(ret)
+sample = np.linspace(ret.min(), ret.max(), len(ret))
+cum = list()
+for i in sample:
+    cum.append(e_cum(i))
+cum = pd.Series(cum)
+
+retR = prediction
+e_cumR = ECDF(retR)
+sampleR = np.linspace(retR.min(), retR.max(), len(retR))
+cumR = list()
+for i in sampleR:
+    cumR.append(e_cumR(i))
+cumR = pd.Series(cumR)
+
+plt.figure(figsize=(12, 4))
+plt.plot(cum, color = 'blue')
+plt.plot(cumR, color = 'red')
+plt.show()
+
+cumulativeR = pd.concat([cum, cumR], axis = 1).set_axis(['returns', 'random'], axis = 1)
 
 
 
